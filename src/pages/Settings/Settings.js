7320d15ae13ff1492874bbe1d3fcd0756b964e5b
Johnlin <johno1001741060168@gmail.com>
2025-04-16 13:21:17 +0800

    Enhanced Settings Page with Structured Layout and Developer Information
    Feature Enhancement/Addition:
    - Introduced a structured settings list on the Settings page, improving user navigation and overall page organization.
    - Added new developer information section, providing users with version details, links to the open source project, developer contacts, and email information. This transparency facilitates user engagement and provides direct channels for feedback or contributions.
    - Implemented a footer section with copyright information, reinforcing brand identity and copyright notice.
    
    UI Adjustments:
    - Defined new CSS rules for the `.settings-list` to enforce a column layout, include appropriate spacing, styling, ensuring a cleaner and more organized visual structure.
    - Added specific styling for `.settings-list-group` and `.settings-list-group-item`, including flex layout for alignment and handling of borders to visually separate items clearly.
    - Styled `.developer-info` block with padding, alignment, and responsive font size adjustments to improve readability and accessibility on various devices.
    - Updated `.copyright` styling with a fixed position to make it persistently visible at the bottom of the page, enhancing the overall layout consistency.
    
    Commit Summary:
    This commit revamps the Settings page by adding structured information blocks like settings options and developer details, alongside clear and organized CSS enhancements. These updates not only improve the visual appeal but also increase the page's functionality and user interaction, supporting a more engaging user experience.

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -5,7 +5,73 @@ function Settings() {
     <div className="page">
       <div className="page-container">
         <h1>設定</h1>
-        <p>暫時還沒有東西呢！</p>
+
+        <div className="settings-list">
+          <div className="settings-list-group">
+            <div className="settings-list-group-item">
+              <p>這裡還沒有東西呢！</p>
+            </div>
+            <div className="settings-list-group-item">
+              <p></p>
+            </div>
+            <div className="settings-list-group-item">
+              <p></p>
+            </div>
+          </div>
+          {/* <div className="settings-list-group">
+            <div className="settings-list-group-item">
+              <p></p>
+            </div>
+            <div className="settings-list-group-item">
+              <p></p>
+            </div>
+            <div className="settings-list-group-item">
+              <p></p>
+            </div>
+          </div> */}
+        </div>
+
+        {/* 開發者資訊 */}
+        <div className="developer-info">
+          <p>版本：1.0.3</p>
+          <p>
+            開放原始碼：
+            <a
+              href="https://github.com/johnlin10/practions"
+              className="no-style"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://github.com/johnlin10/practions
+            </a>
+          </p>
+          <p>
+            開發者：
+            <a
+              href="https://github.com/johnlin10"
+              className="no-style"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              John Lin
+            </a>
+          </p>
+          <p>
+            聯絡方式：
+            <a
+              href="mailto:johnlin@johnlin.me"
+              className="no-style"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              johnlin@johnlin.me
+            </a>
+          </p>
+        </div>
+
+        <div className="copyright">
+          <p>© 2025 Practions. All rights reserved.</p>
+        </div>
       </div>
     </div>
   )
